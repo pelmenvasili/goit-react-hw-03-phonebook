90addef43f50c30b62cc03b81cf0e06d7871780d
Myroslava Lukash <miralukash@gmail.com>
2023-04-23 20:14:42 +0300

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -34,7 +34,7 @@ class ContactForm extends Component {
               className={css.input}
               type="text"
               name="name"
-              pattern="/^[a-zA-Zа-яА-Я]+([ '-][a-zA-Zа-яА-Я]+)*$/"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               value={name}
@@ -47,7 +47,7 @@ class ContactForm extends Component {
               className={css.input}
               type="tel"
               name="number"
-              pattern="/\+?\d{1,4}[-.\s()]?\d{1,3}[-.\s()]?\d{1,4}[-.\s()]?\d{1,4}[-.\s()]?\d{1,9}/"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               value={number}
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
